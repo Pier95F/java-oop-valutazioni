eb0c4512a74afdebd5c60c982115a14b27b53283
Pier95F <forcinapierpaolo@gmail.com>
2022-06-16 15:47:36 +0200

    aggiunto attributi, costruttori e metodi

--- src/jana60/valutazioni/Studente.java
+++ src/jana60/valutazioni/Studente.java
@@ -2,4 +2,30 @@ package jana60.valutazioni;
 
 public class Studente {
 
+	// Definisco gli attributi della classe
+	int idStudente;
+	int percentualeAssenze;
+	double mediaVoti;
+	
+	// Costruttori 
+	public Studente(int idStudente, int percentualeAssenze, double mediaVoti) {
+		super();
+		this.idStudente = idStudente;
+		this.percentualeAssenze = percentualeAssenze;
+		this.mediaVoti = mediaVoti;
+	}
+	
+	// Metodi 
+	boolean promosso() {
+		boolean promosso;
+		if (percentualeAssenze>50) { // se lo studente ha fatto più del 50% di assenze
+			promosso = false;
+		} else if (percentualeAssenze <= 50 && percentualeAssenze >=25) { // se ha fatto tra il 25% e il 50% di assenze
+			promosso = mediaVoti >2;
+		} else if (percentualeAssenze < 25) { // se ha fatto meno del 25% di assenze
+			promosso = mediaVoti >= 2;
+		} else {
+			promosso = false;
+		} return promosso;
+	}
 }
